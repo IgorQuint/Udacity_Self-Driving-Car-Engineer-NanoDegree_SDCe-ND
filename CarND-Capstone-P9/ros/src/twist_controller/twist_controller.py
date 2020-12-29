5e16e5cef42517dcb436b6b9fcfeb44f7b5a9f3c
IgorQuint <igorquint@gmail.com>
2020-12-29 16:11:13 +0100

    Update twistcontroller en drive by wire node

--- CarND-Capstone-P9/ros/src/twist_controller/twist_controller.py
+++ CarND-Capstone-P9/ros/src/twist_controller/twist_controller.py
@@ -1,14 +1,83 @@
+# Let's import all the libraries and modules I will be using for this
+import rospy
+# First the Yaw controller
+from yaw_controller import YawController
+# The PID controller
+from pid import PID
+# The Low Pass Filter
+from lowpass import LowPassFilter
 
 GAS_DENSITY = 2.858
 ONE_MPH = 0.44704
 
-
 class Controller(object):
-    def __init__(self, *args, **kwargs):
+    def __init__(self,vehicle_mass, fuel_capacity , brake_deadband, decel_limit, accel_limit, 
+                 wheel_radius, wheel_base, steer_ratio, max_lat_accel,max_steer_angle):
         # TODO: Implement
-        pass
-
-    def control(self, *args, **kwargs):
+        # I will be using the provided controllers for now
+        min_speed = 0.1
+        self.yaw_controller = YawController(wheel_base, steer_ratio, min_speed, max_lat_accel, max_steer_angle)
+        
+        # Now, for the PID controller I need the Proportional, Integer and Derivative constants
+        kp = 0.3
+        ki = 0.1
+        kd = 0.
+        # Also, I need the min and max values, in this case for Throttle
+        min_throttle = 0.
+        max_throttle = 0.2
+        self.throttle_controller = PID(kp, ki, kd, min_throttle, max_throttle)
+        
+        # Now the lowpass filter for high frequency noise
+        tau = 0.5 #1/(2pi*tau) = cutoff frequency
+        ts = 0.02 #sample time
+        self.vel_lpf = LowPassFilter(tau, ts)
+        
+        self.vehicle_mass = vehicle_mass
+        self.fuel_capacity = fuel_capacity
+        self.brake_deadband = brake_deadband
+        self.decel_limit = decel_limit
+        self.accel_limit = accel_limit
+        self.wheel_radius = wheel_radius
+        
+        self.last_time = rospy.get_time()
+        
+    def control(self, current_vel, dbw_enabled, linear_vel, angular_vel):
         # TODO: Change the arg, kwarg list to suit your needs
         # Return throttle, brake, steer
-        return 1., 0., 0.
+        # First, I dbw_enabled is not True, the car will NOT be controlled by the controller.
+        if not dbw_enabled:
+            self.throttle_controller.reset()
+            return 0., 0., 0.
+        
+        # use the Low Pass Filter in the current velocity
+        current_vel = self.vel_lpf.filt(current_vel)
+        
+        # Time to control the car Steering using the defined yaw controller
+        steering = self.yaw_controller.get_steering(linear_vel, angular_vel, current_vel)
+        #let's see if it works
+        #return 1., 0., steering
+        
+        vel_error = linear_vel - current_vel
+        self.last_vel = current_vel
+        
+        current_time = rospy.get_time()
+        sample_time = current_time - self.last_time
+        self.last_time = current_time
+        
+        throttle = self.throttle_controller.step(vel_error, sample_time)
+        brake = 0
+        
+        # If the "goal" velocity is 0 (maybe a red traffic light), I will send a brake transition
+        # First, If I am near 0 velocity, apply the MAX brake force to make the car totally stops
+        if linear_vel == 0. and current_vel < 0.1:
+            throttle = 0
+            brake = 700 # N*m this is the required torque necessaty for Carla to totally brake.
+        # If I am still going to fast, I will slowly decrese the car velocity    
+        elif throttle < .1 and vel_error < 0:
+            throttle = 0
+            decel = max(vel_error, self.decel_limit) # make the car not Hard Brake, too uncomfortable
+            brake = abs(decel) * self.vehicle_mass*self.wheel_radius # Torque N*m
+            
+        return throttle, brake, steering
+                 
+        #return 1., 0., 0.  #Forcing values to see if the car moves in the simulator, just to test
--- CarND-Capstone-P9/ros/src/waypoint_updater/waypoint_updater.py
+++ CarND-Capstone-P9/ros/src/waypoint_updater/waypoint_updater.py
@@ -3,25 +3,27 @@
 import rospy
 from geometry_msgs.msg import PoseStamped
 from styx_msgs.msg import Lane, Waypoint
+# for the traffic_waypoint
+from std_msgs.msg import Int32
+
+# For locating the closest waypoint in space
+from scipy.spatial import KDTree
 
 import math
+import numpy as np
 
 '''
 This node will publish waypoints from the car's current position to some `x` distance ahead.
-
 As mentioned in the doc, you should ideally first implement a version which does not care
 about traffic lights or obstacles.
-
 Once you have created dbw_node, you will update this node to use the status of traffic lights too.
-
 Please note that our simulator also provides the exact location of traffic lights and their
 current status in `/vehicle/traffic_lights` message. You can use this message to build this node
 as well as to verify your TL classifier.
-
 TODO (for Yousuf and Aaron): Stopline location for each traffic light.
 '''
 
-LOOKAHEAD_WPS = 200 # Number of waypoints we will publish. You can change this number
+LOOKAHEAD_WPS = 50 # Number of waypoints we will publish. You can change this number
 
 
 class WaypointUpdater(object):
@@ -32,22 +34,76 @@ class WaypointUpdater(object):
         rospy.Subscriber('/base_waypoints', Lane, self.waypoints_cb)
 
         # TODO: Add a subscriber for /traffic_waypoint and /obstacle_waypoint below
-
+        # Traffic waypoint has a int 32 message type, so I need to import that message type above
+        rospy.Subscriber('/traffic_waypoint', Int32, self.traffic_cb)
+        rospy.Subscriber('/obstacle_waypoint', Int32, self.obstacle_cb)
 
         self.final_waypoints_pub = rospy.Publisher('final_waypoints', Lane, queue_size=1)
 
         # TODO: Add other member variables you need below
+        self.pose = None
+        self.base_waypoints = None
+        self.waypoints_2d = None
+        self.waypoint_tree = None
+        
+        self.loop()
+    
+    # We use a loop fuction to have control over the publishing frequency
+    def loop(self):
+        rate = rospy.Rate(10) # I can try different rates, down to 30 Hz
+        while not rospy.is_shutdown():
+            if self.pose and self.base_waypoints:
+                # Here I make I have the car position and waypoints before the next functions
+                # I have some trouble where my waypoint_tree is None, and the query function returns an error <<< IMPORTANT 
+                
+                # Get closest waypoint
+                closest_waypoint_idx = self.get_closest_waypoint_idx()
+                self.publish_waypoints(closest_waypoint_idx)
+            rate.sleep()
+            
+     # Let's define the two functions I need in the loop function
+    def get_closest_waypoint_idx(self):
+        x = self.pose.pose.position.x
+        y = self.pose.pose.position.y
+        closest_idx = self.waypoint_tree.query([x, y], 1)[1]
+        
+        # Check to see if the point is ahead or behind
+        closest_coord = self.waypoints_2d[closest_idx]
+        prev_coord = self.waypoints_2d[closest_idx - 1]
 
-        rospy.spin()
+        # Equation for hyperplane through closest_coords
+        cl_vect = np.array(closest_coord)
+        prev_vect = np.array(prev_coord)
+        pos_vect = np.array([x, y])
+        
+        # Based on the explanation in the "Waypoint Updater Partial Walkthrough" min 10
+        val = np.dot(cl_vect - prev_vect, pos_vect - cl_vect)
+        if val > 0:
+            closest_idx = (closest_idx + 1) % len(self.waypoints_2d)
+        return closest_idx
 
+    def publish_waypoints(self, closest_idx):
+        lane = Lane()
+        #lane.header = self.base_waypoints.header
+        lane.waypoints = self.base_waypoints.waypoints[closest_idx:closest_idx + LOOKAHEAD_WPS]
+        self.final_waypoints_pub.publish(lane)
+        
     def pose_cb(self, msg):
         # TODO: Implement
-        pass
+        # Just assign the incoming position data to the internal variable pose
+        self.pose = msg
 
     def waypoints_cb(self, waypoints):
         # TODO: Implement
-        pass
+        # Let's begin with just passing the coming waypoints
+        self.base_waypoints = waypoints
+        if not self.waypoints_2d:
+            # get X and Y points from the waypoints and add them to a tree structure from KDTree
+            self.waypoints_2d = [[waypoint.pose.pose.position.x, waypoint.pose.pose.position.y] for waypoint in waypoints.waypoints]
+            self.waypoint_tree = KDTree(self.waypoints_2d)
 
+    ##### So far, What I have above is all I need to see the waypoints. This is the partial Waypoint Updater :) ######
+    
     def traffic_cb(self, msg):
         # TODO: Callback for /traffic_waypoint message. Implement
         pass
